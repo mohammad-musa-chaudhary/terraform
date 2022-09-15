b76b85a6d2f044a59bb53420327e5690fd6d8e48
Muhammad Chaudhry <M.Chaudhry@midisglobal.local>
2022-09-15 10:49:50 +0300

    changes

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
     resource_group_name  = "example-resources"
     storage_account_name = "teststoragefordeployment"
     container_name       = "test"
-    key                  = "avnet.tfstate"
+    key                  = "project.tfstate"
   }
 
 }
