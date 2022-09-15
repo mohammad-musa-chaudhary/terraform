691660e4b424c33f890080640583c17194d0fbb4
Muhammad Chaudhry <M.Chaudhry@midisglobal.local>
2022-09-15 09:22:48 +0300

    changes

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 
-   used  to put the state file in the storage 
+   #used  to put the state file in the storage 
     backend "azurerm" {
     resource_group_name  = "example-resources"
     storage_account_name = "teststoragefordeployment"
@@ -14,10 +14,25 @@ terraform {
     key                  = "avnet.tfstate"
   }
 
-
 }
 
 provider "azurerm" {
   # Configuration options
   features {}
 }
+
+# virtual network 
+resource "azurerm_virtual_network" "example-network" {
+  name                = "example-virtualnetwork"
+  resource_group_name = var.RegourceGropName  {
+  location            = var.locationVariable 
+  address_space       = ["10.0.0.0/16"]
+
+    subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+}
+}
+
+# 
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable RegourceGropName  {
+  type        = string
+  default     = "example-resources"
+  description = "Regource Grop Name n"
+}
+
+variable locationVariable {
+  type        = string
+  default     = "UAE North"
+  description = "description"
+}
