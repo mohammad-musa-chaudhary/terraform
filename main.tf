dd6f62c02be4534c5d3b792e87e0b750a30bc685
Muhammad Chaudhry <M.Chaudhry@midisglobal.local>
2022-09-22 11:29:29 +0300

    changes

--- main.tf
+++ main.tf
@@ -135,131 +135,170 @@ provider "azurerm" {
 /*                                     Create Kubernetes cluster                                */
 
 
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
-  location            = var.locationVariable
-  resource_group_name = var.RegourceGropName
-  dns_prefix          = "exampleaks1"
+# resource "azurerm_kubernetes_cluster" "example" {
+#   name                = "example-aks1"
+#   location            = var.locationVariable
+#   resource_group_name = var.RegourceGropName
+#   dns_prefix          = "exampleaks1"
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
-  }
+#   default_node_pool {
+#     name       = "default"
+#     node_count = 1
+#     vm_size    = "Standard_D2_v2"
+#   }
 
-  identity {
-    type = "SystemAssigned"
-  }
+#   identity {
+#     type = "SystemAssigned"
+#   }
 
-  tags = {
-    Environment = "Production"
-  }
-}
+#   tags = {
+#     Environment = "Production"
+#   }
+# }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
-  sensitive = true
-}
+# output "client_certificate" {
+#   value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+#   sensitive = true
+# }
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+# output "kube_config" {
+#   value = azurerm_kubernetes_cluster.example.kube_config_raw
 
-  sensitive = true
-}
+#   sensitive = true
+# }
 
 
 /*                                 Create Application Gateway                                  */
 
+# resource "azurerm_virtual_network" "example" {
+#   name                = "example-network"
+#   resource_group_name = var.RegourceGropName
+#   location            = var.locationVariable
+#   address_space       = ["10.254.0.0/16"]
+# }
+
+# resource "azurerm_subnet" "frontend" {
+#   name                 = "frontend"
+#   resource_group_name  = var.RegourceGropName
+#   virtual_network_name = azurerm_virtual_network.example.name
+#   address_prefixes     = ["10.254.0.0/24"]
+# }
+
+# resource "azurerm_subnet" "backend" {
+#   name                 = "backend"
+#   resource_group_name  = var.RegourceGropName
+#   virtual_network_name = azurerm_virtual_network.example.name
+#   address_prefixes     = ["10.254.2.0/24"]
+# }
+
+# resource "azurerm_public_ip" "example" {
+#   name                = "example-pip"
+#   resource_group_name = var.RegourceGropName
+#   location            = var.locationVariable
+#   allocation_method   = "Dynamic"
+# }
+
+# #&nbsp;since these variables are re-used - a locals block makes this more maintainable
+# locals {
+#   backend_address_pool_name      = "${azurerm_virtual_network.example.name}-beap"
+#   frontend_port_name             = "${azurerm_virtual_network.example.name}-feport"
+#   frontend_ip_configuration_name = "${azurerm_virtual_network.example.name}-feip"
+#   http_setting_name              = "${azurerm_virtual_network.example.name}-be-htst"
+#   listener_name                  = "${azurerm_virtual_network.example.name}-httplstn"
+#   request_routing_rule_name      = "${azurerm_virtual_network.example.name}-rqrt"
+#   redirect_configuration_name    = "${azurerm_virtual_network.example.name}-rdrcfg"
+# }
+
+# resource "azurerm_application_gateway" "network" {
+#   name                = "example-appgateway"
+#   resource_group_name = var.RegourceGropName
+#   location            = var.locationVariable
+
+#   sku {
+#     name     = "Standard_Small"
+#     tier     = "Standard"
+#     capacity = 2
+#   }
+
+#   gateway_ip_configuration {
+#     name      = "my-gateway-ip-configuration"
+#     subnet_id = azurerm_subnet.frontend.id
+#   }
+
+#   frontend_port {
+#     name = local.frontend_port_name
+#     port = 80
+#   }
+
+#   frontend_ip_configuration {
+#     name                 = local.frontend_ip_configuration_name
+#     public_ip_address_id = azurerm_public_ip.example.id
+#   }
+
+#   backend_address_pool {
+#     name = local.backend_address_pool_name
+#   }
+
+#   backend_http_settings {
+#     name                  = local.http_setting_name
+#     cookie_based_affinity = "Disabled"
+#     path                  = "/path1/"
+#     port                  = 80
+#     protocol              = "Http"
+#     request_timeout       = 60
+#   }
+
+#   http_listener {
+#     name                           = local.listener_name
+#     frontend_ip_configuration_name = local.frontend_ip_configuration_name
+#     frontend_port_name             = local.frontend_port_name
+#     protocol                       = "Http"
+#   }
+
+#   request_routing_rule {
+#     name                       = local.request_routing_rule_name
+#     rule_type                  = "Basic"
+#     http_listener_name         = local.listener_name
+#     backend_address_pool_name  = local.backend_address_pool_name
+#     backend_http_settings_name = local.http_setting_name
+#   }
+# }
+
+
+/*                                          Creat Firewall                                */
+
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = var.RegourceGropName
+  name                = "testvnet"
+  address_space       = ["10.0.0.0/16"]
   location            = var.locationVariable
-  address_space       = ["10.254.0.0/16"]
+  resource_group_name = var.RegourceGropName
 }
 
-resource "azurerm_subnet" "frontend" {
-  name                 = "frontend"
+resource "azurerm_subnet" "example" {
+  name                 = "AzureFirewallSubnet"
   resource_group_name  = var.RegourceGropName
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.254.0.0/24"]
-}
-
-resource "azurerm_subnet" "backend" {
-  name                 = "backend"
-  resource_group_name  = var.RegourceGropName
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.254.2.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "example" {
-  name                = "example-pip"
-  resource_group_name = var.RegourceGropName
+  name                = "testpip"
   location            = var.locationVariable
-  allocation_method   = "Dynamic"
+  resource_group_name = var.RegourceGropName
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
-#&nbsp;since these variables are re-used - a locals block makes this more maintainable
-locals {
-  backend_address_pool_name      = "${azurerm_virtual_network.example.name}-beap"
-  frontend_port_name             = "${azurerm_virtual_network.example.name}-feport"
-  frontend_ip_configuration_name = "${azurerm_virtual_network.example.name}-feip"
-  http_setting_name              = "${azurerm_virtual_network.example.name}-be-htst"
-  listener_name                  = "${azurerm_virtual_network.example.name}-httplstn"
-  request_routing_rule_name      = "${azurerm_virtual_network.example.name}-rqrt"
-  redirect_configuration_name    = "${azurerm_virtual_network.example.name}-rdrcfg"
-}
-
-resource "azurerm_application_gateway" "network" {
-  name                = "example-appgateway"
-  resource_group_name = var.RegourceGropName
+resource "azurerm_firewall" "example" {
+  name                = "testfirewall"
   location            = var.locationVariable
+  resource_group_name = var.RegourceGropName
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
 
-  sku {
-    name     = "Standard_Small"
-    tier     = "Standard"
-    capacity = 2
-  }
-
-  gateway_ip_configuration {
-    name      = "my-gateway-ip-configuration"
-    subnet_id = azurerm_subnet.frontend.id
-  }
-
-  frontend_port {
-    name = local.frontend_port_name
-    port = 80
-  }
-
-  frontend_ip_configuration {
-    name                 = local.frontend_ip_configuration_name
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.example.id
     public_ip_address_id = azurerm_public_ip.example.id
-  }
-
-  backend_address_pool {
-    name = local.backend_address_pool_name
-  }
-
-  backend_http_settings {
-    name                  = local.http_setting_name
-    cookie_based_affinity = "Disabled"
-    path                  = "/path1/"
-    port                  = 80
-    protocol              = "Http"
-    request_timeout       = 60
-  }
-
-  http_listener {
-    name                           = local.listener_name
-    frontend_ip_configuration_name = local.frontend_ip_configuration_name
-    frontend_port_name             = local.frontend_port_name
-    protocol                       = "Http"
-  }
-
-  request_routing_rule {
-    name                       = local.request_routing_rule_name
-    rule_type                  = "Basic"
-    http_listener_name         = local.listener_name
-    backend_address_pool_name  = local.backend_address_pool_name
-    backend_http_settings_name = local.http_setting_name
   }
 }
